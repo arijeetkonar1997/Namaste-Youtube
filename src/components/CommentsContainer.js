49346967620e44de604877dfa54980a2f1edf838
Arijeet Konar <43075962+arijeetkonar1997@users.noreply.github.com>
2023-05-19 20:15:45 +0530

    Nested Comments

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,114 @@
+import React from 'react'
+
+const commentsData = [
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [
+        {
+          name: "Arijeet Konar",
+          text: "Lorem ipsum dolor sit amet, consectetur adip",
+          replies: [],
+        },
+        {
+          name: "Arijeet Konar",
+          text: "Lorem ipsum dolor sit amet, consectetur adip",
+          replies: [
+            {
+              name: "Arijeet Konar",
+              text: "Lorem ipsum dolor sit amet, consectetur adip",
+              replies: [
+                {
+                  name: "Arijeet Konar",
+                  text: "Lorem ipsum dolor sit amet, consectetur adip",
+                  replies: [
+                    {
+                      name: "Arijeet Konar",
+                      text: "Lorem ipsum dolor sit amet, consectetur adip",
+                      replies: [
+                        {
+                          name: "Arijeet Konar",
+                          text: "Lorem ipsum dolor sit amet, consectetur adip",
+                          replies: [],
+                        },
+                      ],
+                    },
+                    {
+                      name: "Arijeet Konar",
+                      text: "Lorem ipsum dolor sit amet, consectetur adip",
+                      replies: [],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+    {
+      name: "Arijeet Konar",
+      text: "Lorem ipsum dolor sit amet, consectetur adip",
+      replies: [],
+    },
+  ];
+
+const Comment=({data})=>{
+    const {name,text,replies} = data;
+    return (
+        <div className='flex shadow-sm bg-gray p-2 rounded-lg'>
+            <img
+                className="w-12 h-12"
+                src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+                alt="user"
+            />
+            <div className="px-3">
+            <p className="px-3">
+                <p className='font-bold'>{name}</p>
+                <p>{text}</p>
+            </p>
+        </div>
+
+    </div>
+    );
+};
+
+const CommentsList = ({comments})=>{
+    return comments.map((comment, index) => (
+        <div key={index}>
+          <Comment data={comment} />
+          <div className="pl-5 border border-l-black ml-5">
+            <CommentsList comments={comment.replies} />
+          </div>
+        </div>
+      ));
+};
+const CommentsContainer=()=>{
+    return (
+        <div className='m-5 p-2'>
+            <h1 className='text-2xl font-bold'>Comments:</h1>
+            <CommentsList comments={commentsData} />
+        </div>
+    );
+};
+  export default CommentsContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
   const dispatch = useDispatch();
@@ -19,7 +20,11 @@ const WatchPage = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       ></iframe>
+      <div>
+      <CommentsContainer />
     </div>
+    </div>
+    
   );
 };
 
